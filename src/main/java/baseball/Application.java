9b41967a9d2f877905f216016ae94fbbeaeac824
skarltjr <kisa0828@naver.com>
2021-11-10 01:11:26 +0900

    feat : add initial settings

---
+++ src/main/java/baseball/Application.java
@@ -0,0 +1,9 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+    }
+}
