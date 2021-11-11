0b4b1951f792b4ebb7d1800f001864c5d7ca2a88
skarltjr <kisa0828@naver.com>
2021-11-11 23:01:11 +0900

    chore : add settings

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,18 @@
 package baseball;
 
+import baseball.check.ContinueChecker;
+import baseball.game.Game;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        ContinueChecker continueChecker = new ContinueChecker(scanner);
+        do {
+            Game game = new Game(scanner);
+            game.start();
+        } while (continueChecker.checkIfContinue());
+        System.out.println("종료합니다");
     }
 }
